fa2d0da8d1a0e96b86be8b291e10269befc30be8
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-05-13 16:30:02 +0530

    Change CEO Image location

--- src/Mock-data.tsx
+++ src/Mock-data.tsx
@@ -6,7 +6,8 @@ export interface CEOMessageConfig {
 
 export const ceoMessage: CEOMessageConfig = {
   name: "Mr. Ajith Weerasinghe",
-  image: "/ceo.jpg",
+  image:
+    "https://sgp1.digitaloceanspaces.com/cic-react-redesign/images/about-us/10ajith.webp",
   messages: [
     "Our journey is one of purpose and progress. As we embrace new challenges, I am confident that the dedication of our people and the strength of our values will continue to drive CIC towards a future of lasting impact.",
     "The heart of CIC has always been its people. Every day, I am inspired by the commitment and passion of our teams across the island, working together to make a meaningful difference in the communities we serve.",
