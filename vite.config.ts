3aaa4a92487138cf60fc4a4d7fb8e2f8ac4604c6
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-05-14 14:00:49 +0530

    Edit Vite Config FIle

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "dist", // _redirects must end up inside here after build
   },
